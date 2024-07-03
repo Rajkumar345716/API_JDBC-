7a84e60b9affefc72c0bfa0440ba170f653f05e4
Rajkumar345716 <rajkumarvenkat91@gmail.com>
2024-07-03 11:11:38 +0530

    Start

---
+++ src/org/test/ExecutionJDBC.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ExecutionJDBC {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr",
+				"Rajkumar@14");
+		String s = "select * from employees";
+		PreparedStatement preparedStatement = connection.prepareStatement(s);
+		ResultSet executeQuery = preparedStatement.executeQuery();
+		while (executeQuery.next()) {
+			String string = executeQuery.getString("last_name");
+			System.out.println(string);
+		}
+	}
+}
